bcfa60ee569b4fca1f77182ca2dc48b09f3b7bf5
Mario Sumariva <mariosumd@gmail.com>
2016-06-04 22:25:35 +0200

    Termina buscar

--- application/models/General.php
+++ application/models/General.php
@@ -8,15 +8,12 @@ class General extends CI_Model{
     }
 
     function buscar($busqueda) {
-        /*
         $res = $this->db->query("select *
                                    from v_juegos_finalizados
-                                  where nombre_juego like '%?%'", array($busqueda));
-*/
-        $this->db->like('nombre_juego', $busqueda);
-        $res = $this->db->get('v_juegos_finalizados');
+                                  where upper(nombre_juego) like upper(concat('%', ?, '%'))",
+                                  array($busqueda));
 
-        if ($res->num_rows < 1) return array('display' => FALSE);
+        if ($res->num_rows() < 1) return array('display' => FALSE);
         else {
             return array(
                 'display' => TRUE,
